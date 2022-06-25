410f89b28e62a77848ea985371904ad01a9c24fc
BRIAN MASON <bmasonn@yahoo.com>
2022-06-24 20:39:56 -0400

    first commit

---
+++ src/week0/week0Demo.java
@@ -0,0 +1,26 @@
+package week0;
+
+public class week0Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PrintAmessage("Hi to the new cohort");
+		PrintAmessage("Hello All");
+		int message = 6;
+		PrintAmessage(message);
+		int numberStudents = 36;
+		PrintAmessage(numberStudents);
+		
+
+		
+	}
+
+	private static void PrintAmessage(String message) {
+		// TODO Auto-generated method stub
+		System.out.println(message);
+	}
+	private static void PrintAmessage(int message) {
+		// TODO Auto-generated method stub
+		System.out.println(message);
+	}
+}
